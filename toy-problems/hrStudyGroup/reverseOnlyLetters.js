85bff4c940006242482f02e183069dd874d58afd
Zach <zachary.bretz@gmail.com>
2021-09-15 09:00:40 -0600

    Trying second appraoch

--- toy-problems/hrStudyGroup/reverseOnlyLetters.js
+++ toy-problems/hrStudyGroup/reverseOnlyLetters.js
@@ -12,9 +12,42 @@
 // Output: "dc-ba"
 // Example 2:
 
+// d------cba
+
 // Input: s = "a-bC-dEf-ghIj"
 // Output: "j-Ih-gfE-dCba"
 // Example 3:
 
 // Input: s = "Test1ng-Leet=code-Q!"
 // Output: "Qedo1ct-eeLg=ntse-T!"
+
+// a = [a,b,c,d]
+// a.length = 4
+// a.first = 0 (or index)
+// a.last = length -1 - a.first (or index)
+
+//still constant time (2n) to remove special chars and then iterate
+
+var alphabet = 'abcdefghijklmnopqrstuvwxy'.split('')
+var leftSpecial = 0
+var rightSpecial = 0
+
+// const reverse = (str) => {
+//   str = str.split('')
+//   for (let i = 0; i < str.length; i++){
+//     if (!alphabet.includes(str[i + leftSpecial])){
+//       leftSpecial++
+//     }
+//     if (!alphabet.includes(str[str.length - 1 - i])){
+//       rightSpecial++
+//     }
+//   }
+// }
+
+const reverse = (str) => {
+  str = str.split('')
+  newStr = []
+  for (let i = 0; i < str.length; i++){
+
+  }
+}
