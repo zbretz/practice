f60355a1466e2f9aea25160b3f1c4d8e0add8c7b
Zach <zachary.bretz@gmail.com>
2021-09-15 10:22:57 -0600

    Finished solution

--- toy-problems/hrStudyGroup/reverseOnlyLetters.js
+++ toy-problems/hrStudyGroup/reverseOnlyLetters.js
@@ -28,9 +28,12 @@
 
 //still constant time (2n) to remove special chars and then iterate
 
-var alphabet = 'abcdefghijklmnopqrstuvwxy'.split('')
-var leftSpecial = 0
-var rightSpecial = 0
+var alphabet = 'abcdefghijklmnopqrstuvwxy'
+var upperAlphabet = alphabet.toUpperCase()
+alphabet = alphabet + upperAlphabet
+alphabet = alphabet.split('')
+// var leftSpecial = 0
+// var rightSpecial = 0
 
 // const reverse = (str) => {
 //   str = str.split('')
@@ -44,10 +47,42 @@ var rightSpecial = 0
 //   }
 // }
 
+// even
+// abcd
+// stopping point is < (length-1)/2
+
+// odd
+// abcde
+// stopping point is < (length-1)/2
+
+
+//second approach
 const reverse = (str) => {
   str = str.split('')
-  newStr = []
+  let newStr = []
+  let specialChars = {}
   for (let i = 0; i < str.length; i++){
-
+    if (alphabet.includes(str[i])){
+      newStr.push(str[i])
+    } else {
+      specialChars[i] = str[i]
+    }
   }
+  str = newStr
+  const length = newStr.length
+  const stoppingPoint = (length/2) - 1
+  for (let i = 0; i <= stoppingPoint; i++){
+    [newStr[i],newStr[length - 1 - i]] = [newStr[length - 1 - i],newStr[i]]
+  }
+  for (key in specialChars){
+    newStr.splice(key, 0, specialChars[key])
+    console.log(key)
+  }
+  return newStr.join('')
 }
+console.log(reverse('Test1ng-Leet=code-Q!'))
+
+//notes:
+//determining length and stopping point outside the loop saves calculations
+//destructuring to swap
+//0(3n) reduces to 0(n)
