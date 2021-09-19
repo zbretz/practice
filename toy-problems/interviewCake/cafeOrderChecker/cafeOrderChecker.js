6c0329cc35b622dfd9c7ae2885b3e2bce02aa388
Zach <zachary.bretz@gmail.com>
2021-09-18 23:18:43 -0600

    Added solution

--- toy-problems/interviewCake/cafeOrderChecker/cafeOrderChecker.js
+++ toy-problems/interviewCake/cafeOrderChecker/cafeOrderChecker.js
@@ -16,3 +16,26 @@
 //   Served Orders: [17, 8, 12, 19, 24, 2]
 // would be first-come, first-served.
 
+const checker = (take, dine, served) => {
+
+  while(served.length){
+  // console.log(served.length)
+  const last = served[served.length-1]
+    if (last === take[take.length-1]){
+      served.pop()
+      take.pop()
+    } else if(last === dine[dine.length-1]){
+      served.pop()
+      dine.pop()
+    } else {
+      return false
+    }
+  }
+
+  return true
+}
+
+const first = checker([1, 3, 5],[2, 4, 6],[1, 2, 4, 6, 5, 3])
+const second = checker([17, 8, 24],[12, 19, 2], [17, 8, 12, 19, 24, 2])
+console.log(first)
+console.log(second)
