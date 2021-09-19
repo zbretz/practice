7d8082c3f0dea8e6ffb07cbea415af87ca209bc9
Zach <zachary.bretz@gmail.com>
2021-09-19 12:36:01 -0600

    Reconfigured Mocha testing directory to run tests

---
+++ Mocha/test/sqliteMocha.spec.js
@@ -0,0 +1,34 @@
+const assert = require("assert");
+const {
+  initDB,
+  createTable,
+  dropTable,
+  insertUser,
+  getUsersByName,
+  closeDBPool,
+} = require("../src/sqliteIndex.js");
+
+describe("Sqlite", function () {
+  let db;
+  before((done) => {
+    db = initDB(':memory:');
+    db.serialize(async function() {
+      await dropTable(db);
+      await createTable(db);
+      done();
+    });
+  });
+
+  it("should insert and fetch a user", async () => {
+    const name = "mocha";
+    const email = "mocha@test.com";
+
+    await insertUser(db, name, email);
+    const user = await getUsersByName(db, name);
+    assert.deepEqual(user, [{ id: 1, name, email }]);
+  });
+
+  after(() => {
+    closeDBPool(db);
+  });
+});
