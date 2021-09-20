a819af252d04ad1323e33ff3d85ccac461cd968c
Zach <zachary.bretz@gmail.com>
2021-09-19 18:50:35 -0600

    Added second range solution (without inner function)

--- toy-problems/recursionPractice/range.js
+++ toy-problems/recursionPractice/range.js
@@ -1,13 +1,25 @@
+// const range = (x,y) => {
+//   let arr = []
+//   inner = function(x,y){
+//     x++;
+//     if (x > y) return
+//     arr.push(x)
+//     inner(x,y)
+//     }
+//   inner(x,y)
+//   return arr
+// }
+// console.log(range(2,9))
+
+second mathod
+var arr = []
+
 const range = (x,y) => {
-  let arr = []
-  inner = function(x,y){
-    x++;
-    if (x > y) return
-    arr.push(x)
-    inner(x,y)
-    }
-  inner(x,y)
+  if (x === y) return
+  x++;
+  arr.push(x)
+
+  range(x,y)
   return arr
 }
-
 console.log(range(2,9))
