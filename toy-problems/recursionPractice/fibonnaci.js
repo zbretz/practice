a6ddcaa0558c29a06f623f7e546b39cbfd24b7e4
Zach <zachary.bretz@gmail.com>
2021-09-20 09:58:17 -0600

    Implemented fibonacci solution to produce value at index = n

---
+++ toy-problems/recursionPractice/fibonnaci.js
@@ -0,0 +1,16 @@
+// Write a JavaScript program to get the first n Fibonacci numbers. Go to the editor
+// Note : The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, . . . Each subsequent number is the sum of the previous two.
+
+const fib = (n) => {
+  if (n <= 1) return 0;
+  if (n === 2) return 1;
+
+  return fib(n-1) + fib(n-2)
+}
+
+console.log(fib(1))
+console.log(fib(2))
+console.log(fib(3))
+console.log(fib(4))
+console.log(fib(5))
+
