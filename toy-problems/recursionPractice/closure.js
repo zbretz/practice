6efe098ca3f608a0c4b72eb7a36b714b68097cda
Zach <zachary.bretz@gmail.com>
2021-09-19 19:23:41 -0600

    Added second closure example

--- toy-problems/recursionPractice/closure.js
+++ toy-problems/recursionPractice/closure.js
@@ -1,7 +1,5 @@
 const print3Times = () => {
-
   x = 0
-
   const printThis = (str) => {
     x++
     if (x < 4)  console.log(str)
@@ -9,7 +7,17 @@ const print3Times = () => {
   return printThis
 }
 
-print3Times('hello')
-print3Times('hello')
-print3Times('hello')
-print3Times('hello')
+var test = print3Times()
+test('first')
+test('second')
+test('third')
+test('noprint')
+
+const add = (x) => {
+  return function(y){
+    return x + y
+  }
+}
+
+var add10 = add(10)
+console.log(add10(5))
