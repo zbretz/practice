13871d2e80b1c12769748a1c9176d4e66fe4d551
Zach <zachary.bretz@gmail.com>
2021-09-20 12:35:19 -0600

    Implemented second solution to simple count

--- toy-problems/recursionPractice/simpleCount.js
+++ toy-problems/recursionPractice/simpleCount.js
@@ -1,13 +1,25 @@
 //write function to count from 0 to n
-let number = 0
 
-var count = (n) => {
-  if (number === n){
+// let number = 0
+// var count = (n) => {
+//   if (number === n){
+//     return
+//   }
+//   console.log(number)
+//   number++
+//   count(n)
+// }
+// count(6)
+
+
+var count = (n, counter) => {
+
+  counter = counter ? counter : 0
+  if (counter === n){
     return
   }
-  console.log(number)
-  number++
-  count(n)
+  console.log(counter)
+  counter++
+  count(n, counter)
 }
 count(6)
-// console.log(count(6))
