eac9113547de42675a8711bfe9aa1c078ed739c2
Zach <zachary.bretz@gmail.com>
2021-09-23 15:17:44 -0600

    Added a few more practice examples and comments

--- toy-problems/setAndSpread.js
+++ toy-problems/setAndSpread.js
@@ -1,7 +1,29 @@
+//creating a set using new keyword
 var firstSet = new Set('abcdab')
 console.log(firstSet)
+
+//using the spread operator
 var arr1 = [1,2,3]
 var arr2 = [...arr1, 4,5]
 console.log(arr2)
+//spreading a set:
 alphaArr = [...firstSet]
 console.log(alphaArr)
+//using 'add' method
+firstSet.add('a')
+console.log(firstSet)
+
+//this is cool:
+arr3 = [1,1,1]
+arr4 = arr3
+arr4[0] = 9
+console.log(arr3) //[9,1,1]
+//however with spread operator:
+arr3 = [1,1,1]
+arr4 = [...arr3]
+arr4[0] = 9
+console.log(arr3) //[1,1,1]
+
+//sets have 'size' not 'length'
+console.log(firstSet.size)
+console.log(firstSet.length)
