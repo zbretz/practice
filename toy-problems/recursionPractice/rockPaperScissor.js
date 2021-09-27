061154e73c0fe9237bdb2a7a364df755cae0dc3a
Zach <zachary.bretz@gmail.com>
2021-09-27 13:47:02 -0600

    Completed solution (needed to initialize a new array to build off with each recursive loop

--- toy-problems/recursionPractice/rockPaperScissor.js
+++ toy-problems/recursionPractice/rockPaperScissor.js
@@ -1,18 +1,20 @@
 const rps = (rounds) => {
 
   const rps_string = ['r','p','s']
-  const arr = ['r','p','s']
+  let arr = ['r','p','s']
   if (rounds === 0) return []
 
   const inner = (rounds) => {
     if (rounds === 1) return
 
+    let newArr = []
     arr.forEach(i => (
       rps_string.forEach(j => (
-        arr.push(i+j)
+        newArr.push(i+j)
       )
     )
     ))
+    arr = newArr
     inner(rounds-1)
   }
 
