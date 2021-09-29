ad864ba5410b63d570de1396121852de0c8253d5
Zach <zachary.bretz@gmail.com>
2021-09-28 22:23:16 -0600

    Fixed memo function for proper memoization

--- toy-problems/interviewCake/memoization/memoFib.js
+++ toy-problems/interviewCake/memoization/memoFib.js
@@ -1,15 +1,37 @@
 //fibonacci with memoization
+const {performance} = require('perf_hooks');
 
+let memo = []
 const fibMem = (n) => {
-   const memo = {}
+  // const memo = []
   if (n==0) return 0
   if (n==1) return 1
 
   if (memo[n]) {
-    return fibMem(n)
+    return memo[n]
   } else {
-    return fibMem(n-1) + fibMem(n-2)
+    memo[n] = fibMem(n-1) + fibMem(n-2)
   }
+  return memo[n]
 }
 
+
+var fibStart = performance.now()
 console.log(fibMem(6))
+var fibEnd = performance.now()
+
+console.log(`Call to fibMem took ${fibEnd - fibStart} milliseconds`)
+
+const fib = (n) => {
+  if (n==0) return 0
+  if (n==1) return 1
+
+  return fib(n-1) + fib(n-2)
+}
+
+var memStart = performance.now()
+console.log(fib(3))
+var memEnd = performance.now()
+
+console.log(`Call to fib took ${memEnd - memStart} milliseconds`)
+
