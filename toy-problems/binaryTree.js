c71eea3ad2b3494fdd487b3a41bf0f209b80efea
Zach <zachary.bretz@gmail.com>
2021-09-29 13:14:07 -0600

    Finally solved this problem

--- toy-problems/binaryTree.js
+++ toy-problems/binaryTree.js
@@ -24,6 +24,7 @@ leftright = firstleft.insertRight(8)
 rightleft = firstright.insertLeft(10)
 rightright = firstright.insertRight(12)
 
+
 //func(tree)
 //if tree.left !== null
 //func(tree.left)
@@ -39,19 +40,70 @@ rightright = firstright.insertRight(12)
 //function to find greatest depth and return array of its values
 
 //function to generate all arrays
+// let container =[];
+
+// const printNodesArrays = (tree) => {
+
+//   if (!(tree.left || tree.right)) return [tree.value]
+
+//   let arr = [];
+
+//   if (tree.left) arr = arr.concat([tree.value].concat(printNodesArrays(tree.left)))
+
+//   if (tree.right) arr = arr.concat([tree.value].concat(printNodesArrays(tree.right)))
+
+//   container.push(arr)
+
+//   return container
+// }
+
+// console.log(printNodesArrays(myTree))
+
+
+
 let container =[];
 
-const printNodesArrays = (tree) => {
-  let arr = [];
+const printNodesArrays = (tree, arr=[]) => {
+  arr.push(tree.value)
 
-  if (!(tree.left || tree.right)) return [tree.value]
 
-  if (tree.left) arr = arr.concat([tree.value].concat(printNodesArrays(tree.left)))
-  if (tree.right) arr = arr.concat([tree.value].concat(printNodesArrays(tree.right)))
 
-  container.push(arr)
+  if (!(tree.left || tree.right)) console.log(arr)
 
-  return container
+  if (tree.left){
+    printNodesArrays(tree.left, arr.slice())
+  }
+  if (tree.right){
+    printNodesArrays(tree.right, arr.slice())
+  }
+
+  // returnsole.log container
 }
 
 console.log(printNodesArrays(myTree))
+
+
+// const newAttempt = (tree) => {
+//   let arr = []
+//   let path = ''
+
+//     const traverse = (node,path) => {
+//       if (!node.left && !node.right){
+//         arr.push(path)
+//         return
+//       }
+
+//       if (node.left){
+//         path += node.left.value
+//         traverse(node.left,path)
+//       }
+//       if (node.right){
+//         path += node.right.value
+//         traverse(node.right,path)
+//       }
+//     }
+//     traverse(tree,path=String(tree.value))
+//     return arr
+// }
+
+// console.log(newAttempt(myTree))
