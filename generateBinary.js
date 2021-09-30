b7ed42db7767ae4d084ddfa409c1d6578b631af5
Zach <zachary.bretz@gmail.com>
2021-09-29 21:37:15 -0600

    Integrated the helper into main function

--- generateBinary.js
+++ generateBinary.js
@@ -1,3 +1,16 @@
+// let powers = [4,2]
+
+const convertPowers = (arr) => {
+  let binary = 0;
+  arr.forEach(num => {
+    // num = num === 1? num : num-1
+    return binary += 10**(num-1)
+  })
+  console.log(binary)
+}
+
+// convertPowers(powers)
+
 const generateBinary = (num) => {
   let remainder = num;
   let divisor = 0;
@@ -13,25 +26,14 @@ const generateBinary = (num) => {
       // if (remainder % 2 !== 0) break
       n++
       remainder = num - 2**n
-
     }
-    powers.push(n)
+    //n + 1 below to handle eg. 2**2 puts '1' in third 'place'
+    powers.push(n + 1)
     inner(num-2**n)
   }
   inner(num)
-  console.log(powers)
+  convertPowers(powers)
 }
 
 generateBinary(38)
 
-let powers = [4,2]
-
-const convertPowers = (arr) => {
-  let binary = 0;
-  arr.forEach(num => {
-    return binary += 10**(num-1)
-  })
-  console.log(binary)
-}
-
-convertPowers(powers)
