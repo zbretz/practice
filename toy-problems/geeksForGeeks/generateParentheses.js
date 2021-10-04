e456940aecbdee30e99abeab984e42ffaa77d835
Zach <zachary.bretz@gmail.com>
2021-10-04 15:28:41 -0600

    Solved

--- toy-problems/geeksForGeeks/generateParentheses.js
+++ toy-problems/geeksForGeeks/generateParentheses.js
@@ -11,8 +11,68 @@
 // {{}}
 // Explanation: This the only two sequences of balanced arenthesis formed using 2 pair of balanced arenthesis.
 
+// const result = []
+
 const genParens = (n) =>{
+  let result = []
+  // let string = '';
+
+  const inner = (string ='', left = 0, right = 0)=> {
+    let newString;
+
+    // if (left === right && right === n){
+    if (right === n){
+      result.push(string)
+    }
+
+    if (left < n){
+      // ++left
+      inner(string + '(', left + 1, right)
+
+    }
+
+    if (right < left){
+      // ++right
+      inner(string + ')', left, right + 1)
+    }
+  }
+
+  inner()
+  console.log(result)
 
 }
 
-generateParens(4)
+genParens(3)
+
+
+
+// const genParens = (n) =>{
+
+//   let string = '';
+
+//   const inner = (left = 0, right = 0)=> {
+
+//     if (left === right && right === n){
+//       console.log(string)
+//       return ''
+//     }
+
+//     if (left < n){
+//       string += '(';
+//       left++
+//       inner(left, right)
+//     }
+
+//     if (right < left){
+//       string += ')';
+//       right++
+//       inner(left, right)
+//     }
+//     return string
+//   }
+
+//   inner()
+
+// }
+
+// genParens(2)
