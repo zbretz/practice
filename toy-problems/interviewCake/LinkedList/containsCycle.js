6d23099f316d03e15b28f279f8f2ee4031525c4c
Zach <zachary.bretz@gmail.com>
2021-10-13 08:28:30 -0600

    Added second solution

--- toy-problems/interviewCake/LinkedList/containsCycle.js
+++ toy-problems/interviewCake/LinkedList/containsCycle.js
@@ -8,10 +8,16 @@ class LinkedListNode {
 const a = new LinkedListNode('A')
 const b = new LinkedListNode('B')
 const c = new LinkedListNode('C')
+const d = new LinkedListNode('D')
+const e = new LinkedListNode('E')
+
 
 a.next = b;
 b.next = c;
-c.next = a;
+c.next = d;
+d.next = e;
+e.next = a;
+
 
 const containsCycle = (node) => {
   const history = []
@@ -32,3 +38,23 @@ const containsCycle = (node) => {
 }
 
 console.log(containsCycle(a))
+
+//two runners
+const cycleRunners = (node) => {
+  let fast_runner = node;
+  let slow_runner = node;
+  let count = 0;
+  while(true){
+    console.log(count, fast_runner.value, slow_runner.value)
+    count++
+    fast_runner = fast_runner.next
+    if (count % 2 === 0) slow_runner = slow_runner.next
+    if (fast_runner === slow_runner){
+      console.log(count, fast_runner.value, slow_runner.value)
+      return true
+    }
+    if (fast_runner.next === null) return false
+  }
+}
+
+console.log(cycleRunners(a))
