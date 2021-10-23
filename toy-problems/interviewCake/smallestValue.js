ab2951d2b4db67ce47117ddfebbd76a525cbd1bc
Zach <zachary.bretz@gmail.com>
2021-10-22 23:49:09 -0600

    Added prompt

---
+++ toy-problems/interviewCake/smallestValue.js
@@ -0,0 +1,11 @@
+// Smallest Non-constructible Value
+// Given an array of positive integers (representing coins), find the smallest value that cannot be constructed from those integers.
+// Reference: Elements of Programming Interviews in python, page 189
+// Example 1:
+// Input: A = [1, 2, 4]
+// Output: 8
+// Explanation: With subsets of A, we can construct values 1, 2, 3, 4, 5, 6, 7.
+// Example 2:
+// Input: A = [1, 2, 5]
+// Output: 4
+// Explanation: With subsets of A, we can construct values 1, 2, 3, 5, 6, 7, 8.
