653b8eab304d73931422eefad89ee9ca29f0fd97
Zach <zachary.bretz@gmail.com>
2021-10-27 23:01:28 -0600

    Simulated db connection with simple module

---
+++ express-demo/db.js
@@ -0,0 +1,1 @@
+module.exports = {'user1':{'first':'zach','last':'bretz'},'user2':{'first':'luis', 'last':'castro'}}
--- express-demo/server.js
+++ express-demo/server.js
@@ -2,6 +2,8 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const db = require('./db')
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
