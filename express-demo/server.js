12144f4fcf110b87068cca18a70e20efec4fe21c
Zach <zachary.bretz@gmail.com>
2021-10-27 23:00:27 -0600

    bare-bones server

---
+++ express-demo/server.js
@@ -0,0 +1,16 @@
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/', (request, response) => {
+  response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
