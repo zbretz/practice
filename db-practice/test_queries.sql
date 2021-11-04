c90a3621ddb8bc294cacb1316479fdce51c8b07c
Zach <zachary.bretz@gmail.com>
2021-11-03 22:46:23 -0600

    Added snome queries

--- db-practice/test_queries.sql
+++ db-practice/test_queries.sql
@@ -10,3 +10,9 @@ SELECT * FROM location_media WHERE type='video' ORDER BY RANDOM() LIMIT 1;
 UPDATE location_media SET $1 = $2 WHERE id=$3
 DELETE FROM location_media WHERE id = $1
 
+-- Snome
+INSERT INTO location_media(location, type, filename, caption)
+VALUES($1, $2, $3, $4)
+SELECT * FROM location_media WHERE type='video' ORDER BY RANDOM() LIMIT 1;
+UPDATE location_media SET $1 = $2 WHERE id=$3
+DELETE FROM location_media WHERE id = $1
