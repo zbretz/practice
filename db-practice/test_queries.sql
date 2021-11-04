4b7c34d7a2a2da889dd9dd0b54c61c93e6ea48fc
Zach <zachary.bretz@gmail.com>
2021-11-03 22:45:59 -0600

    Added media queries

--- db-practice/test_queries.sql
+++ db-practice/test_queries.sql
@@ -2,3 +2,11 @@
 SELECT * FROM users WHERE location = $1
 LIST VIEW: SELECT name, cover_image FROM location WHERE featured = true
 MAP VIEW: SELECT longitude, latitude FROM location LIMIT 6
+
+-- Media
+INSERT INTO location_media(location, type, filename, caption)
+VALUES($1, $2, $3, $4)
+SELECT * FROM location_media WHERE type='video' ORDER BY RANDOM() LIMIT 1;
+UPDATE location_media SET $1 = $2 WHERE id=$3
+DELETE FROM location_media WHERE id = $1
+
