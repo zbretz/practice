5b15f8d467e54953b8d6ebb5ba76c2c1858ea71b
Zach <zachary.bretz@gmail.com>
2021-11-03 22:47:00 -0600

    Added profile queries

--- db-practice/test_queries.sql
+++ db-practice/test_queries.sql
@@ -16,3 +16,10 @@ VALUES($1, $2, $3, $4)
 SELECT * FROM location_media WHERE type='video' ORDER BY RANDOM() LIMIT 1;
 UPDATE location_media SET $1 = $2 WHERE id=$3
 DELETE FROM location_media WHERE id = $1
+
+-- Profile Queries
+INSERT INTO user (location_id, name, travel_start, travel_end, about, video_tour)
+VALUES ($1, $2, $3, $4, $5, $6, $7)
+SELECT * FROM user
+UPDATE user SET $1 = $2 WHERE id =$3
+DELETE FROM user WHERE id = $1
