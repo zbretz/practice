6666ce508355d09fa44c1444b58df750e8609ff7
Zach <zachary.bretz@gmail.com>
2021-11-18 11:55:20 -0700

    Testing styling wwqith percentage dimensions

--- react-native/AwesomeProject/App.js
+++ react-native/AwesomeProject/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const FlexDimensionsBasics = () => {
+const PercentageDimensionsBasics = () => {
+  // Try removing the `height: '100%'` on the parent View.
+  // The parent will not have dimensions, so the children can't expand.
   return (
-    // Try removing the `flex: 1` on the parent View.
-    // The parent will not have dimensions, so the children can't expand.
-    // What if you add `height: 300` instead of `flex: 1`?
-    <View style={{ flex: 1 }}>
-      <View style={{ flex: 1, backgroundColor: 'powderblue' }} />
-      <View style={{ flex: 2, backgroundColor: 'skyblue' }} />
-      <View style={{ flex: 3, backgroundColor: 'steelblue' }} />
+    <View style={{ height: '100%' }}>
+      <View style={{
+        height: '15%', backgroundColor: 'powderblue'
+      }} />
+      <View style={{
+        width: '66%', height: '35%', backgroundColor: 'skyblue'
+      }} />
+      <View style={{
+        width: '33%', height: '50%', backgroundColor: 'steelblue'
+      }} />
     </View>
   );
 };
 
-export default FlexDimensionsBasics;
+export default PercentageDimensionsBasics;
