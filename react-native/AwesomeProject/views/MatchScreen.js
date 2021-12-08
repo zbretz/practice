f9c88b67478c1d5878a5d4e94b7a2fc410f31c92
Zach <zachary.bretz@gmail.com>
2021-12-08 09:29:56 -0700

    Employ Flat List to display matches

--- react-native/AwesomeProject/views/MatchScreen.js
+++ react-native/AwesomeProject/views/MatchScreen.js
@@ -1,61 +1,32 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import {Dimensions} from 'react-native';
 
+const DATA = [{location: "parkcity"}, {location: "aspen"}, {location: "crestedbutte"}, {location: "alta"}]
+
+const Item = ({ location }) => (
+  <View style={styles.item}>
+    <Text style={styles.location}>{location}</Text>
+  </View>
+);
+
 const MatchScreen = () => {
-  const [flexDirection, setflexDirection] = useState("column");
+
+  const renderItem = ({ item }) => (
+    <Item location={item.location} />
+  );
 
   return (
-    <Grid
-      values={["parkcity", "aspen", "crestedbutte", "alta"]}
-      selectedValue={flexDirection}
-      setSelectedValue={setflexDirection}
-    >
-    </Grid>
+    <SafeAreaView>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.location}
+      />
+    </SafeAreaView>
   );
 };
 
-const Grid = ({
-  label,
-  children,
-  values,
-  selectedValue,
-  setSelectedValue,
-}) => (
-  <View style={{ padding: 10, flex: 1 }}>
-
-  {/* Top Container */}
-  <View style={[styles.topContainer]}>
-  {children}
-    <View>
-      <Text style={styles.SnomeLogo}>Snome Logo</Text>
-    </View>
-  </View>
-
-    <View style={styles.row}>
-      {values.map((value) => (
-        <TouchableOpacity
-          key={value}
-          onPress={() => setSelectedValue(value)}
-          style={[
-            styles.location,
-            selectedValue === value && styles.selected,
-          ]}
-        >
-          <Text
-            style={[
-              styles.buttonLabel,
-              selectedValue === value && styles.selectedLabel,
-            ]}
-          >
-            {value}
-          </Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-
-  </View>
-);
 
 const styles = StyleSheet.create({
   SnomeLogo: {
@@ -76,26 +47,11 @@ const styles = StyleSheet.create({
     marginTop: 8,
     backgroundColor: "aliceblue",
   },
-  box: {
-    width: 50,
-    height: 50,
-  },
-  row: {
-    // flex:1,
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-evenly",
-  },
   location: {
-    // paddingHorizontal: 8,
-    // paddingVertical: 6,
-    // flex: 1,
     borderRadius: 4,
     backgroundColor: "oldlace",
-    // alignSelf: "flex-start",
-    // marginHorizontal: "1%",
     marginBottom: 16,
-    width: Dimensions.get('window').width * 0.4,
+    // width: Dimensions.get('window').width * 0.4,
     height: Dimensions.get('window').width * 0.4,
     textAlign: "center",
   },
@@ -103,19 +59,7 @@ const styles = StyleSheet.create({
     backgroundColor: "coral",
     borderWidth: 0,
   },
-  buttonLabel: {
-    fontSize: 12,
-    fontWeight: "500",
-    color: "coral",
-  },
-  selectedLabel: {
-    color: "white",
-  },
-  label: {
-    textAlign: "center",
-    marginBottom: 10,
-    fontSize: 24,
-  },
+
 });
 
 export default MatchScreen
