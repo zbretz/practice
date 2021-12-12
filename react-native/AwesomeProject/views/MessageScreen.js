ed7c4361e610a90de962fd7051042906a50f9342
Zach <zachary.bretz@gmail.com>
2021-12-12 13:07:22 -0700

    Message identifies recipient based on selected message. Input hides until a thread(conversation) is selected

--- react-native/AwesomeProject/views/MessageScreen.js
+++ react-native/AwesomeProject/views/MessageScreen.js
@@ -13,6 +13,7 @@ import {View, Text, TextInput, StyleSheet, SafeAreaView, SectionList, ScrollView
 // Next Steps:
 // - Expand message to display more text
 // - select message thread -> input text -> appear message
+// - Incorporate context to identify user and then insert user_id into messages/create message
 
 const messages = [
   {
@@ -24,14 +25,14 @@ const messages = [
     has_been_read: true
   },
   {id:2,recipient:6,sender:1,time:'1999-01-08 4:06:06',message_text:"you're gonna love it",has_been_read:true},
-  // {id:3,recipient:1,sender:6,time:'1999-01-08 4:07:06',message_text:"when do you want to come?",has_been_read:true},
-  // {id:4,recipient:6,sender:7,time:'1999-01-08 4:08:06',message_text:"i'll recommend some bars",has_been_read:true},
-  // {id:5,recipient:7,sender:6,time:'1999-01-08 4:09:06',message_text:"can you recommend some restaurants?",has_been_read:true},
-  // {id:6,recipient:6,sender:1,time:'1999-01-08 4:10:06',message_text:"are you really close to the mountain?",has_been_read:true},
-  // {id:7,recipient:5,sender:6,time:'1999-01-08 4:11:06',message_text:"let's connect soon",has_been_read:true},
-  // {id:8,recipient:6,sender:5,time:'1999-01-08 4:12:06',message_text:"can I extend my stay?",has_been_read:false},
-  // {id:9,recipient:5,sender:6,time:'1999-01-08 4:13:06',message_text:"have you been in town before?",has_been_read:false},
-  // {id:10,recipient:1,sender:6,time:'1999-01-08 4:14:06',message_text:"see you soon!",has_been_read:false}
+  {id:3,recipient:1,sender:6,time:'1999-01-08 4:07:06',message_text:"when do you want to come?",has_been_read:true},
+  {id:4,recipient:6,sender:7,time:'1999-01-08 4:08:06',message_text:"i'll recommend some bars",has_been_read:true},
+  {id:5,recipient:7,sender:6,time:'1999-01-08 4:09:06',message_text:"can you recommend some restaurants?",has_been_read:true},
+  {id:6,recipient:6,sender:1,time:'1999-01-08 4:10:06',message_text:"are you really close to the mountain?",has_been_read:true},
+  {id:7,recipient:5,sender:6,time:'1999-01-08 4:11:06',message_text:"let's connect soon",has_been_read:true},
+  {id:8,recipient:6,sender:5,time:'1999-01-08 4:12:06',message_text:"can I extend my stay?",has_been_read:false},
+  {id:9,recipient:5,sender:6,time:'1999-01-08 4:13:06',message_text:"have you been in town before?",has_been_read:false},
+  {id:10,recipient:1,sender:6,time:'1999-01-08 4:14:06',message_text:"see you soon!",has_been_read:false}
 ]
 
 const MessageCard = ({message, selectedUser, setSelectedUser, selectedRecipient, setSelectedRecipient, findOtherUser, otherUser}) => {
@@ -75,7 +76,8 @@ const MessageCard = ({message, selectedUser, setSelectedUser, selectedRecipient,
 const styles = {
   this_user: {
     borderColor: '#1e90ff',
-    textAlign: 'right'
+    textAlign: 'right',
+
   },
   card: {
     margin:4,
@@ -95,6 +97,7 @@ const styles = {
     borderColor: '#e1861b',
     padding: 10,
     backgroundColor: "white",
+    display:'hide'
   }
 };
 
@@ -119,11 +122,11 @@ const MessageScreen = () => {
   const createMessage = () => {
 
     console.log('ghghghg')
-    let thing = messages2.slice()
-    thing.push(
+    // let thing = messages2.slice()
+    messages2.unshift(
       {
         id:10,
-        recipient:1,
+        recipient:otherUser,
         sender:6,
         time:'1999-01-08 4:14:06',
         message_text:newMessage,
@@ -141,7 +144,7 @@ const MessageScreen = () => {
     //   }
     // )
     // setMessages2(messages2)
-    setMessages2(thing)
+    setMessages2(messages2)
     setNewMessage("")
     console.log(newMessage)
     Keyboard.dismiss()
@@ -149,13 +152,15 @@ const MessageScreen = () => {
 
   return (
     <>
- <Text>{newMessage}</Text>
+    <Text style={{height:40, borderWidth:2, borderColor:'gold'}}>{otherUser}</Text>
+    <Text>{newMessage}</Text>
     <FlatList
       data={messages2}
       renderItem={renderItem}
       keyExtractor={item => item.message_text}
       extraData={messages2}
     />
+    {otherUser &&
     <View>
       <TextInput
         // these two lines screwed up setNewMesssage. be careful using them!
@@ -167,6 +172,7 @@ const MessageScreen = () => {
         onSubmitEditing = {()=>createMessage()}
         />
     </View>
+    }
     </>
   );
 };
