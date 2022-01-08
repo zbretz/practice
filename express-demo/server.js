ac138465a7aef92e5fac5b1f7e3ef91b1d798745
Zach <zachary.bretz@gmail.com>
2022-01-07 21:55:29 -0700

    Added some experiments for JSON Web Tokens (JWT)

--- express-demo/server.js
+++ express-demo/server.js
@@ -2,10 +2,21 @@ const express = require('express')
 const app = express()
 const port = 3000
 const db = require('./db')
+const jwt = require("jsonwebtoken");
 
 
 app.get('/', (request, response) => {
-  response.send(db.user1.first)
+  let token1 = request.header('Authorization');
+  console.log(token1)
+  const token2 = jwt.sign('abc', '123');
+
+  response.header("auth-token", token2).send(db.user1.first)
+})
+
+app.get('/test', (request, response) => {
+  let token1 = request.header('Authorization');
+  console.log(token1)
+  response.send(token1)
 })
 
 app.listen(port, () => {
