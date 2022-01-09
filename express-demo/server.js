047d1ffc50a2fa21a05da0378570dddd7c153275
Zach <zachary.bretz@gmail.com>
2022-01-09 01:16:13 -0700

    Init basic jwt test code/app

--- express-demo/server.js
+++ express-demo/server.js
@@ -1,23 +1,36 @@
+// https://medium.com/@ryanchenkie_40935/react-authentication-how-to-store-jwt-in-a-cookie-346519310e81
+// https://www.codementor.io/@manashkumarchakrobortty/authentication-and-authorization-in-node-js-19brdvhsyw
+
 const express = require('express')
 const app = express()
 const port = 3000
-const db = require('./db')
-const jwt = require("jsonwebtoken");
+const db = require('./db');
+const jwt = require('express-jwt');
+const jsonwebtoken = require('jsonwebtoken');
+const cors = require('cors');
+
+app.use(cors());
 
 
-app.get('/', (request, response) => {
-  let token1 = request.header('Authorization');
-  console.log(token1)
-  const token2 = jwt.sign('abc', '123');
-
-  response.header("auth-token", token2).send(db.user1.first)
+app.get('/jwt', (request, response) => {
+  // let token1 = request.header('Authorization');
+  const token2 = jsonwebtoken.sign('abc', '123');
+console.log(token2)
+  // response.header("auth-token", token2).json({token:token2})
+  // response.json({token:token2})
+  response.send({token:token2})
 })
 
-app.get('/test', (request, response) => {
-  let token1 = request.header('Authorization');
-  console.log(token1)
-  response.send(token1)
-})
+// app.use(jwt({ secret: '123', algorithms: ['HS256'] }));
+const foods = [
+  { id: 1, description: 'burritos' },
+  { id: 2, description: 'quesadillas' },
+  { id: 3, description: 'churos' }
+];
+
+app.get('/foods', (req, res) => {
+  res.json(foods);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
