4ddf2bf7757cb1d27dd73834fc8b08db7d120394
Zach <zachary.bretz@gmail.com>
2022-01-14 17:54:09 -0700

    Add async example

--- AsynchronousJS/async_await.js
+++ AsynchronousJS/async_await.js
@@ -1,9 +1,18 @@
-//work-in-progress
-async function example(){
-  return await new Promise(resolve =>setTimeout(()=> (resolve, console.log('test1'), 4000))
-  )
-  console.log('test2')
+// //work-in-progress
+// async function example(){
+//   return await new Promise(resolve =>setTimeout(()=> (resolve, console.log('test1'), 4000))
+//   )
+//   console.log('test2')
+
+// }
+
+// example()
+
+async function testing (a){
+  if(a) return 'this'
+  return new Error
 
 }
 
-example()
+let data = testing()
+data.then(msg => {console.log(msg)})
