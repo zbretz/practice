4ffb085fd04b41450b6859b6f092e59b37321b6b
Jinhoo Bong <jinhoobong@gmail.com>
2022-01-15 20:23:18 -0800

    rendered simple homepage & implemented css-loader and style-loader as dependencies to load css file

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Homepage from './LandingPage/Homepage.jsx';
 
 const App = () => {
   return (
-  <div>
-    <h1>CommonApp</h1>
-  </div>
+    <div>
+      <h1>CommonApp</h1>
+      <Homepage />
+    </div>
   )
 }
 
---
+++ client/components/LandingPage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './homepage.css';
+
+const Homepage = () => {
+
+ return (
+   <div className='home'>
+     <h1>Common App</h1>
+     <button>asdf</button>
+     <button>asdf</button>
+   </div>
+ )
+}
+
+export default Homepage;
