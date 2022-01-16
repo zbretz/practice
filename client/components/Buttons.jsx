651e27450ea466440fa99c6f7dc098b64f825064
Jinhoo Bong <jinhoobong@gmail.com>
2022-01-15 20:43:39 -0800

    initial homepage set up with 2 buttons

---
+++ client/components/Buttons.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button = ({ text, link}) => {
+
+  return (
+    <button>{text}</button>
+  )
+
+}
+
+export default Button;
--- client/components/LandingPage/Homepage.jsx
+++ client/components/LandingPage/Homepage.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
+import Buttons from '../Buttons.jsx';
+
 import './homepage.css';
 
 const Homepage = () => {
 
  return (
-   <div className='home'>
-     <h1>Apply</h1>
-     <div className='buttons'>
-      
-     </div>
-   </div>
+  <div className='home'>
+    <h1 className='title'>Apply.io</h1>
+      <div className='buttons'>
+        <Buttons text={'Register'} />
+        <Buttons text={'Sign Up'} /> 
+      </div>
+  </div>
  )
 }
 
