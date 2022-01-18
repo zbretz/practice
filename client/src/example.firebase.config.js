414cc7288ccea0ebefdc29355804ba55fe011c1a
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-18 14:42:50 -0800

    Created example firebase config file as a reference for others

---
+++ client/src/example.firebase.config.js
@@ -0,0 +1,11 @@
+// rename this file to firebase.config.js and fill the values for the firebase account
+// ** make sure not to commit these values to the repo (file should be in .gitignore)
+export const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
+};
