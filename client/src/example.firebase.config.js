0b42a58889ad8e9d538c7e16a315f2b3591f6090
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-18 15:57:12 -0800

    Updated instruction comments

--- client/src/example.firebase.config.js
+++ client/src/example.firebase.config.js
@@ -1,5 +1,5 @@
-// rename this file to firebase.config.js and fill the values for the firebase account
-// ** make sure not to commit these values to the repo (file should be in .gitignore)
+// create a file in your local repo called firebase.config.js and fill the values for the firebase account
+// ** make sure not to commit these values to the repo (the file with values should be in the .gitignore)
 export const firebaseConfig = {
   apiKey: "",
   authDomain: "",
