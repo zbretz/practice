e3e9aefc9297ac619e539c961adc2e7bd262141b
Jerick Iquin - Home <jci.dev.0@gmail.com>
2022-01-19 11:44:36 -0800

    added get users by role

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -15,4 +15,36 @@ userControllers.getAllUsers = async (req, res, next) => {
 	}
 };
 
+userControllers.getUserById = async (req, res, next) => {
+	const { id } = req.params;
+	try {
+		const user = await User.findOne({
+			attributes: EXCLUDEDATES,
+			where: {
+				id
+			}
+		});
+		return user ? res.status(200).json(user) : next(createError(404, 'User not found'));
+	} catch (e) {
+		next(createError(500, e));
+	}
+};
+
+userControllers.getUsersByRole = async (req, res, next) => {
+	const { role_id } = req.params;
+	try {
+		const users = await User.findAll({
+			attributes: EXCLUDEDATES,
+			where: {
+				role_id
+			}
+		});
+		return users
+			? res.status(200).json(users)
+			: next(createError(404, 'Users not found with that role'));
+	} catch (e) {
+		next(createError(500, e));
+	}
+};
+
 module.exports = userControllers;
--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,6 +1,9 @@
 const usersRouter = require('express').Router();
-const { getAllUsers } = require('../controllers/users');
+const { getAllUsers, getUserById, getUsersByRole } = require('../controllers/users');
 
 usersRouter.get('/', getAllUsers);
+usersRouter.get('/:id', getUserById);
+
+usersRouter.get('/role/:role_id', getUsersByRole);
 
 module.exports = usersRouter;
