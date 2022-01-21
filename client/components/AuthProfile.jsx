c167d159fc4ad06a33829912ef94efb5ef1a57c6
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-21 11:12:29 -0800

    Created simple AuthProfile componenent for route testing

---
+++ client/components/AuthProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const AuthProfile = () => {
+  return (
+    <div>
+      <h1>Profile</h1>
+      <Link to="/">Sign Up</Link>
+    </div>
+  )
+}
+
+export default AuthProfile
