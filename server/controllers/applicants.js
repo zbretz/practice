474233648b0722b4f126d9f150029b1facce7bc8
Jerick Iquin - Home <jci.dev.0@gmail.com>
2022-01-21 11:45:51 -0800

    initial boilerplate for get request

---
+++ server/controllers/applicants.js
@@ -0,0 +1,18 @@
+const { Applicant } = require('../db/models/index');
+const { EXCLUDEDATES } = require('../utils/db-utils');
+const createError = require('http-errors');
+
+const applicantControllers = {};
+
+applicantControllers.getAllApplicants = async (_req, res, next) => {
+	try {
+		const users = await Applicant.findAll({
+			attributes: EXCLUDEDATES
+		});
+		res.status(200).json(users);
+	} catch (e) {
+		next(createError(500, e));
+	}
+};
+
+module.exports = applicantControllers;
--- server/routes/applicantsRoute.js
+++ server/routes/applicantsRoute.js
@@ -1,3 +1,6 @@
 const applicantsRouter = require('express').Router();
+const { getAllApplicants } = require('../controllers/applicants');
+
+applicantsRouter.get('/', getAllApplicants);
 
 module.exports = applicantsRouter;
