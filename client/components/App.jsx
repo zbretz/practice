1b255fb999ecac6f0d8ff86a9b0e77c41ee70ee5
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-21 11:15:10 -0800

    imported react-router-dom, created a switch with two routes for rout testing

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,12 +1,18 @@
 import React, { useRef } from 'react';
 import { AuthProvider } from '../contexts/AuthContext'
 import AuthTesting from './AuthTesting.jsx'
+import AuthProfile from './AuthProfile.jsx'
+import { Switch, Route, Link } from 'react-router-dom'
 
 const App = () => {
   return (
     <AuthProvider>
-      <h1>CommonApp</h1>
-      <AuthTesting />
+
+      <Switch>
+        <Route exact path="/profile" component={AuthProfile} />
+        <Route exact path="/" component={AuthTesting} />
+      </Switch>
+
     </AuthProvider>
   )
 }
