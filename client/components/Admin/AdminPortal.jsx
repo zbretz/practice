82857a5c31d1f5ad0a8fc6b1a27e533febfbade6
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-25 11:34:54 -0800

    Added Admin Folder with Admin Portal Component Page

---
+++ client/components/Admin/AdminPortal.jsx
@@ -0,0 +1,13 @@
+import React, { useRef } from 'react';
+import { Switch, Route, Link, NavLink } from 'react-router-dom'
+import { Form, Button, Card, Container } from 'react-bootstrap'
+
+const AdminPortal = () => {
+  return (
+    <Container>
+      <h1> Admin Portal </h1>
+    </Container>
+  )
+}
+
+export default AdminPortal
