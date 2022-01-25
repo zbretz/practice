6be8c30e6e6a6bd1e760e9ab3062707f6314c65e
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-25 12:01:53 -0800

    Created recruiter info form that routes to recruiter portal

---
+++ client/components/Recruiter/RecruiterInfoForm.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Form, Button, Card, Container } from 'react-bootstrap'
+import { BrowserRouter, Switch, Route, Link, Redirect, NavLink } from 'react-router-dom'
+
+const RecruiterInfoForm = () => {
+
+  return (
+    <Container>
+
+    <>
+      <Card>
+        <Card.Body>
+          <h1>Recruiter Info Form</h1>
+          <Form>
+            <Form.Group id="job">
+              <Form.Label>Job to Fill</Form.Label>
+              <Form.Control type="text"  required />
+            </Form.Group>
+            <Form.Group id="location">
+              <Form.Label> Location of Job</Form.Label>
+              <Form.Control type="text"  required />
+            </Form.Group>
+          </Form>
+
+          <Link to="/recruiterPortal" >
+          <Button type="submit" >Go To Portal</Button>
+          </Link>
+
+        </Card.Body>
+      </Card>
+    </>
+
+  </Container>
+  )
+
+}
+
+export default RecruiterInfoForm;
