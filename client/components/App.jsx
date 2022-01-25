0e1c3cf9e452615895d07828ee9f236cc6d88a32
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-25 11:20:09 -0800

    Imported portal components and created routes for each one

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,21 +1,36 @@
 import React, { useRef } from 'react';
 import { AuthProvider } from '../contexts/AuthContext'
-import AuthTesting from './AuthTesting.jsx'
-import AuthProfile from './AuthProfile.jsx'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route, Link, NavLink } from 'react-router-dom'
+import { Form, Button, Card, Container } from 'react-bootstrap'
+
 import Homepage from './LandingPage/Homepage.jsx';
+import Login from './LandingPage/Login.jsx';
+import CreateProfilePage from './Applicant/CreateProfilePage.jsx'
+import SignUp from './LandingPage/SignUp.jsx'
+import ChooseAccountType from './ChooseAccountType.jsx'
+import RecruiterPortal from './Recruiter/RecruiterPortal.jsx'
+import ApplicantPortal from './Applicant/ApplicantPortal.jsx'
+import AdminPortal from './Admin/AdminPortal.jsx'
 
 const App = () => {
   return (
-    <AuthProvider>
+    <Container>
+      <NavLink to="/">Back to Homepage</NavLink>
+      <AuthProvider>
 
-      <Switch>
-        <Route exact path="/profile" component={AuthProfile} />
-        <Route exact path="/signUp" component={AuthTesting} />
-        <Route exact path="/" component={Homepage} />
-      </Switch>
+        <Switch>
+          <Route exact path="/recruiterPortal" component={RecruiterPortal} />
+          <Route exact path="/applicantPortal" component={ApplicantPortal} />
+          <Route exact path="/adminPortal" component={AdminPortal} />
+          <Route exact path="/createProfilePage" component={CreateProfilePage} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/signUp" component={SignUp} />
+          <Route exact path="/chooseAccountType" component={ChooseAccountType} />
+          <Route exact path="/" component={Homepage} />
+        </Switch>
 
-    </AuthProvider>
+      </AuthProvider>
+    </Container>
   )
 }
 
