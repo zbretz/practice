ea2dd85ea0cc7592b7886e5108a8c3944727fec8
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-25 11:34:16 -0800

    Added recruiter folder with recruiter portal component page

---
+++ client/components/Recruiter/RecruiterPortal.jsx
@@ -0,0 +1,13 @@
+import React, { useRef } from 'react';
+import { Switch, Route, Link, NavLink } from 'react-router-dom'
+import { Form, Button, Card, Container } from 'react-bootstrap'
+
+const RecruiterPortal = () => {
+  return (
+    <Container>
+      <h1> Recruiter Portal </h1>
+    </Container>
+  )
+}
+
+export default RecruiterPortal
