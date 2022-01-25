0d22a9635d132b7783f17dd87ac4ddf7368f3be1
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-25 11:33:23 -0800

    Added applicant folder with Applicant Portal Component page

---
+++ client/components/Applicant/ApplicantPortal.jsx
@@ -0,0 +1,13 @@
+import React, { useRef } from 'react';
+import { Switch, Route, Link, NavLink } from 'react-router-dom'
+import { Form, Button, Card, Container } from 'react-bootstrap'
+
+const ApplicantPortal = () => {
+  return (
+    <Container>
+      <h1> Applicant Portal </h1>
+    </Container>
+  )
+}
+
+export default ApplicantPortal
