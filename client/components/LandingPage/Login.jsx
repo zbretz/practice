cca2e7db01a26d5d2564909b424d4f34a2989ca0
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-25 11:27:40 -0800

    Converted login to use bootstrap, andimplemented route for user to correct portal based on role

--- client/components/LandingPage/Login.jsx
+++ client/components/LandingPage/Login.jsx
@@ -1,22 +1,63 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Container } from 'react-bootstrap'
+import { useAuth } from '../../contexts/AuthContext.js'
+import { Switch, Route, Link, Redirect, NavLink, useHistory } from 'react-router-dom'
 
 const Login = () => {
+  const history = useHistory()
+  const emailRef = useRef()
+  const passwordRef = useRef()
+  const { signUp, role, isSignedIn, currentUser, userSignOut, signIn } = useAuth()
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    signIn(emailRef.current.value, passwordRef.current.value)
+    console.log('user role is ', role)
+    // issue: requires 2 clicks
+    if (isSignedIn && currentUser) {
+      history.push(`/${role}Portal` || '/portal')
+    }
+
+  }
+
+  const handleSignOut = (e) => {
+    e.preventDefault();
+    userSignOut()
+  }
 
   return (
-    <div>
-      <form>
-        <label>
-          Username:
-          <input type="text"></input>
-        </label>
-        <label>
-          Password:
-          <input type="text"></input>
-        </label>
-      </form>
-    </div>
+    <Container>
+      <Button type="submit" onClick={handleSignOut}>Sign Out</Button>
+
+      <>
+        <Card>
+          <Card.Body>
+            <h1>Log In</h1>
+            {/* {JSON.stringify(currentUser)} */}
+            <Form>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+              </Form.Group>
+            </Form>
+
+            <Button type="button" onClick={handleSignIn}>Log In</Button>
+
+          </Card.Body>
+        </Card>
+      </>
+      <div>
+        Need to make an Account?
+        <Link to="/signUp">  Sign Up</Link>
+      </div>
+
+    </Container>
   )
 
 }
 
-export default Login;
+export default Login
