702fad7c77578443fb2800b002758a475cefa6ec
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-25 16:30:34 -0800

    Imported admin info form and route

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -12,6 +12,7 @@ import ApplicantInfoForm from './Applicant/ApplicantInfoForm.jsx'
 import RecruiterPortal from './Recruiter/RecruiterPortal.jsx'
 import RecruiterInfoForm from './Recruiter/RecruiterInfoForm.jsx'
 import AdminPortal from './Admin/AdminPortal.jsx'
+import AdminInfoForm from './Admin/AdminInfoForm.jsx'
 
 const App = () => {
   return (
@@ -20,11 +21,12 @@ const App = () => {
       <AuthProvider>
 
         <Switch>
+          <Route exact path="/applicantPortal" component={ApplicantPortal} />
+          <Route exact path="/applicantInfoForm" component={ApplicantInfoForm} />
           <Route exact path="/recruiterPortal" component={RecruiterPortal} />
           <Route exact path="/recruiterInfoForm" component={RecruiterInfoForm} />
-          <Route exact path="/applicantPortal" component={ApplicantPortal} />
           <Route exact path="/adminPortal" component={AdminPortal} />
-          <Route exact path="/applicantInfoForm" component={ApplicantInfoForm} />
+          <Route exact path="/adminInfoForm" component={AdminInfoForm} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signUp" component={SignUp} />
           <Route exact path="/chooseAccountType" component={ChooseAccountType} />
