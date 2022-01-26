213ed6782fa64bfc750f358e10d5f506d31236b6
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-25 16:28:46 -0800

    added route back to homepage when recruiter signs out

--- client/components/Recruiter/RecruiterPortal.jsx
+++ client/components/Recruiter/RecruiterPortal.jsx
@@ -1,10 +1,21 @@
 import React, { useRef } from 'react';
-import { Switch, Route, Link, NavLink } from 'react-router-dom'
+import { Switch, Route, Link, NavLink, useHistory } from 'react-router-dom'
 import { Form, Button, Card, Container } from 'react-bootstrap'
+import { useAuth } from '../../contexts/AuthContext.js'
 
 const RecruiterPortal = () => {
+  const history = useHistory()
+  const { userSignOut} = useAuth()
+
+  const handleSignOut = (e) => {
+    e.preventDefault();
+    userSignOut()
+    history.push('/')
+  }
+
   return (
     <Container>
+      <Button type="submit" onClick={handleSignOut}>Sign Out</Button>
       <h1> Recruiter Portal </h1>
     </Container>
   )
