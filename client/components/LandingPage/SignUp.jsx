e366c271940b2c8056725dda57445103ae058d30
Jinhoo Bong <jinhoobong@gmail.com>
2022-01-26 21:55:35 -0800

    created function to check if user enters valid password and alerts for cases

--- client/components/LandingPage/SignUp.jsx
+++ client/components/LandingPage/SignUp.jsx
@@ -12,14 +12,19 @@ const SignUp = () => {
   const verifyPassword = useRef();
 
   const [pwMatch, setPWMatch] = useState(true);
+  const [validPw, setValidPw] = useState(true);
 
   const { signUp, isSignedIn, currentUser, userSignOut, signIn } = useAuth()
 
   const handleSignUp = (e) => {
     e.preventDefault();
 
-    // validPW();
-    signUp(emailRef.current.value, passwordRef.current.value)
+    checkValidPW() ? (
+      signUp(emailRef.current.value, passwordRef.current.value),
+      setValidPw(true)
+    ) : 
+      setValidPw(false)
+    
     // issue: requires 2 clicks
     // if (isSignedIn && currentUser) {
     // history.push('/chooseAccountType')
@@ -34,22 +39,39 @@ const SignUp = () => {
     passwordRef.current.value === verifyPassword.current.value ? setPWMatch(true) : setPWMatch(false);
   }
 
-  const validPW = () => {
-    // password should contain one upper case
-    // one symbol
-    // one number
+  const checkValidPW = () => {
+    var special_chars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
 
     let containsUpper = false;
     let containsSymbol = false;
     let containsNumber = false;
 
     let password = passwordRef.current.value;
+
     for (let i = 0; i < password.length; i++) {
-      if (password[i].isUpperCase()) {
-        console.log('hit');
+      // check to see if it contains uppercase
+      if (/^[A-Z]*$/.test(password[i])) {
+        containsUpper = true;
+      } 
+
+      // check to see if it contains special character
+      if (special_chars.test(password[i])) {
+        // console.log('hi');
+        containsSymbol = true;
+      }
+
+      // check if string contains number
+      if (/\d/.test(password[i])) {
+        containsNumber = true;
       }
     }
-    console.log('not found');
+    
+    if ((containsUpper && containsSymbol) && containsNumber) {
+      return true;
+    } else {
+      return false;
+    }
+
   }
 
   const handleSignOut = (e) => {
@@ -75,11 +97,12 @@ const SignUp = () => {
                 <Form.Label>Password</Form.Label>
                 <Form.Control type="password" ref={passwordRef} required />
               </Form.Group>
+              {validPw ? <Alert variant="secondary">Your password must contain an uppercase letter, a special character, and a number</Alert> : <Alert variant="danger">Invalid Password type</Alert>}
               <Form.Group id="password">
                 <Form.Label>Verify Password</Form.Label>
                 <Form.Control type="password" ref={verifyPassword} required onChange={() => verify()}/>
               </Form.Group>
-              {pwMatch ? <></> : <Alert variant="danger">Incorrect Password Verification</Alert>}
+              {pwMatch ? <></> : <Alert variant="danger">Password doesn't match</Alert>}
             </Form>
             <Button type="submit" onClick={handleSignUp}>Sign Up</Button>
 
