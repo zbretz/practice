66549d707ff39628ce22c799d0b56025dce7dc83
Jinhoo Bong <jinhoobong@gmail.com>
2022-01-27 16:51:51 -0800

    added role choice in form and corresponding event handlers, and created sample post request to create user object and send data to db

--- client/components/Applicant/ApplicantInfoForm.jsx
+++ client/components/Applicant/ApplicantInfoForm.jsx
@@ -2,21 +2,35 @@ import React, { useState } from 'react';
 import { Form, Button, Card, Container } from 'react-bootstrap'
 import { BrowserRouter, Switch, Route, Link, Redirect, NavLink } from 'react-router-dom'
 
+const axios = require('axios');
+
 // Form Component to capture the applicant information
 const ApplicantInfoForm = () => {
 
   // Gather all applicant information
   // send it to database via POST request
+  const postApplicantInfo = () => {
+    //create user object with all information
+    let userObj = {
+      name: name, 
+      email: email, 
+      github: github,
+      linkedIn: linkedIn, 
+      jobPref: jobPref,
+      locationPref: locationPref,
+      resume: resume,
+      role: role
+    };
 
-  // username
-  // full name
-  // email
-  // github link
-  // linkedin link
-  // job desired
-  // location desired (remote, etc)
-  // resume upload
+    console.log(userObj);
 
+    // axios post request to post to backend
+    // axios.post('/user', userObj)
+    //   .then((res) => console.log('User info successfully stored to db'))
+    //   .catch((err) => console.log('Error in saving data to database'));
+  }
+
+  const [role, setRole] = useState('');
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [github, setGithub] = useState('');
@@ -25,39 +39,44 @@ const ApplicantInfoForm = () => {
   const [locationPref, setLocationPref] = useState('');
   const [resume, setResume] = useState({});
 
+  const handleRoleInput = (e) => {
+    // console.log(e.target.value);
+    setRole(e.target.value);
+  }
+
   const handleNameInput = (e) => {
-    console.log('name', e.target.value);
-    // setName(e.target.value);
+    // console.log('name', e.target.value);
+    setName(e.target.value);
   }
 
   const handleEmailInput = (e) => {
-    console.log('email', e.target.value);
-    // setEmail(e.target.value);
+    // console.log('email', e.target.value);
+    setEmail(e.target.value);
   }
 
   const handleGithubInput = (e) => {
-    console.log('github', e.target.value);
-    // setGithub(e.target.value);
+    // console.log('github', e.target.value);
+    setGithub(e.target.value);
   }
 
   const handleLinkedInInput = (e) => {
-    console.log('linkedin', e.target.value);
-    // setLinkedIn(e.target.value);
+    // console.log('linkedin', e.target.value);
+    setLinkedIn(e.target.value);
   }
 
   const handleJobPrefInput = (e) => {
-    console.log('Job', e.target.value);
-    // setJobPref(e.target.value);
+    // console.log('Job', e.target.value);
+    setJobPref(e.target.value);
   }
 
   const handleLocationPref = (e) => {
-    console.log('Location', e.target.value);
-    // setLocationPref(e.target.value);
+    // console.log('Location', e.target.value);
+    setLocationPref(e.target.value);
   }
 
   const handleResume = (e) => {
-    console.log('resume', e.target.files[0]);
-    // setResume(e.target.files[0])
+    // console.log('resume', e.target.files[0]);
+    setResume(e.target.files[0])
   }
 
   return (
@@ -67,6 +86,21 @@ const ApplicantInfoForm = () => {
         <Card.Body>
           <h1>Applicant Info Form</h1>
           <Form>
+            <Form.Group>
+              {/* Figure out how to restrict both options from being selected */}
+              <Form.Check
+                inline
+                label="Applicant"
+                value="Applicant"
+                onChange={(e) => handleRoleInput(e)}
+              />
+              <Form.Check
+                inline
+                label="Recruiter"
+                value="Recruiter"
+                onChange={(e) => handleRoleInput(e)}
+              />
+            </Form.Group>
             <Form.Group id="fullName">
               <Form.Label>Full Name:</Form.Label>
               <Form.Control type="text" onChange={(e) => handleNameInput(e)} required/>
@@ -106,7 +140,7 @@ const ApplicantInfoForm = () => {
           </Form>
 
           <Link to="/applicantPortal" >
-          <Button type="submit">Go To Portal</Button>
+          <Button type="submit" onClick={() => postApplicantInfo()}>Go To Portal</Button>
           </Link>
 
         </Card.Body>
