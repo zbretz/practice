67267d977066727dedda329f2363ff67204a1231
Zach <zachary.bretz@gmail.com>
2022-01-27 19:37:33 -0700

    Inserted landing page into recruiter portal

--- client/components/Recruiter/RecruiterPortal.jsx
+++ client/components/Recruiter/RecruiterPortal.jsx
@@ -2,6 +2,9 @@ import React, { useRef } from 'react';
 import { Switch, Route, Link, NavLink, useHistory } from 'react-router-dom'
 import { Form, Button, Card, Container } from 'react-bootstrap'
 import { useAuth } from '../../contexts/AuthContext.js'
+import Recruiter_Landing from './Recruiter_Landing.jsx'
+import styles from '../styles/recruiterLanding.module.css'
+
 
 const RecruiterPortal = () => {
   const history = useHistory()
@@ -17,6 +20,7 @@ const RecruiterPortal = () => {
     <Container>
       <Button type="submit" onClick={handleSignOut}>Sign Out</Button>
       <h1> Recruiter Portal </h1>
+      <Recruiter_Landing />
     </Container>
   )
 }
