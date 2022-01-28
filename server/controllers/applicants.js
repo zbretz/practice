dcf9ab962ae5660586983ffbfe6625e6ce57015b
Lenora Esquenazi <lenaciousd@gmail.com>
2022-01-28 11:04:37 -0800

    returning hard coded fake data for applicant

--- server/controllers/applicants.js
+++ server/controllers/applicants.js
@@ -16,12 +16,31 @@ applicantControllers.getAllApplicants = async (req, res, next) => {
 applicantControllers.getApplicantById = async (req, res, next) => {
 	try {
 		const { id } = req.params;
-		const applicant = await Applicant.findOne({
-			...joinConfig(User),
-			where: {
-				user_id: id
-			}
-		});
+		// const applicant = await Applicant.findOne({
+		// 	...joinConfig(User),
+		// 	where: {
+		// 		user_id: id
+		// 	}
+		// });
+
+		const applicant =
+		{	id: 1,
+			user_id: "007",
+			experience: "1 year",
+			linkedIn: "fakeperson@linkedin.com",
+			school: "Hack Reactor",
+			desired_position: "Full-stack Developer",
+			desired_industry:"Tech",
+			desired_company_size: "Small",
+			desired_company_stage: "Start Up",
+			desired_region: "Remote",
+			resume: "/myresume/fakeperson",
+			role: "applicant",
+			email: "test5@test.com",
+			password: "yeqpuUbjdsTMYEfD3OcjwEL7AAk1",
+			name: "Bob Saget"
+		}
+
 		return applicant ? res.status(200).json(applicant) : next(notFoundError('Applicant'));
 	} catch (e) {
 		next(createError(500, e));
