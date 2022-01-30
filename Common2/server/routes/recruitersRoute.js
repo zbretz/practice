191e52cadf7bf1a1e617dd756a2cb1d72e82ce3f
Zach <zachary.bretz@gmail.com>
2022-01-29 20:46:21 -0700

    Created mock data for leetcode and react phases to deliver to candidate_view page

--- Common2/server/routes/recruitersRoute.js
+++ Common2/server/routes/recruitersRoute.js
@@ -33,30 +33,56 @@ recruitersRouter.get('/byId', (req, res) => {
 
   console.log(req.query)
 
-  // const fake_user_table = {
-  //   user: 'Zachary Bretz',
-  //   user_id: '2',
-  // }
+  let react_tab = {
+    tab: "React",
+    interviewer_name: "Zachary Bretz",
+    performance_summary: "As fast as thou shalt wane, so fast thou grow'st, In one of thine, from that which thou departest; And that fresh blood which youngly thou bestow'st, Thou mayst call thine when thou from youth convertest, Herein lives wisdom, beauty, and increase; Without this folly, age, and cold decay: If all were minded so, the times should cease And threescore year would make the world away. Let those whom nature hath not made for store, Harsh, featureless, and rude, barrenly perish:",
+    video_file_path: 'video.mp4',
+    evaluation: [
+      {
+      criterion: "Asked Qs when needed",
+      rating: "4"
+      },{
+        criterion: "Met Requirements",
+        rating: "3"
+      },{
+        criterion: "Understood prompt",
+        rating: "4"
+      }
+    ]
+  }
+
+  let leetcode_tab = {
+    tab: "knjksjndkjndfs",
+    interviewer_name: "Zachary Bretz",
+    performance_summary: "klmeflmeflm",
+    video_file_path: 'video.mp4',
+    evaluation: [
+      {
+      criterion: "Asked Qs when needed",
+      rating: "4"
+      },{
+        criterion: "Met Requirements",
+        rating: "3"
+      },{
+        criterion: "Understood prompt",
+        rating: "4"
+      }
+    ]
+  }
+
+  const fake_user_table = {
+    user: 'Zachary Bretz',
+    user_id: '2',
+    interview_phases: {
+      React: react_tab,
+      Leetcode: leetcode_tab
+    }
+
+  }
 
   res.send(
-    {
-      tab: "React",
-      interviewer_name: "Zachary Bretz",
-      performance_summary: "As fast as thou shalt wane, so fast thou grow'st, In one of thine, from that which thou departest; And that fresh blood which youngly thou bestow'st, Thou mayst call thine when thou from youth convertest, Herein lives wisdom, beauty, and increase; Without this folly, age, and cold decay: If all were minded so, the times should cease And threescore year would make the world away. Let those whom nature hath not made for store, Harsh, featureless, and rude, barrenly perish:",
-      video_file_path: 'video.mp4',
-      evaluation: [
-        {
-        criterion: "Asked Qs when needed",
-        rating: "4"
-        },{
-          criterion: "Met Requirements",
-          rating: "3"
-        },{
-          criterion: "Understood prompt",
-          rating: "4"
-        }
-      ]
-    }
+    react_tab
   )
 });
 
