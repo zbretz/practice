8918c5035aebbc76846f63910c37100f8c97ddf2
Zach <zachary.bretz@gmail.com>
2022-01-31 14:29:49 -0700

    Created basic landing page (homepage)

--- Common2/client/components/App.jsx
+++ Common2/client/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { useRef } from 'react';
 import { AuthProvider } from '../contexts/AuthContext'
 import { Switch, Route, Link, NavLink } from 'react-router-dom'
-import { Form, Button, Card, Container } from 'react-bootstrap'
+import { Form, Button, Card, Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
 
 import Homepage from './LandingPage/Homepage.jsx';
 import SignUp from './LandingPage/SignUp.jsx'
@@ -17,9 +17,19 @@ import Recruiter_Candidate_View from './Recruiter/Recruiter_Candidate_View.jsx'
 
 const App = () => {
   return (
+    <Container fluid>
+        <Navbar bg="primary" variant="dark">
     <Container>
-      <NavLink to="/">Back to Homepage</NavLink>
-      <AuthProvider>
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="me-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+
+    <AuthProvider>
 
         <Switch>
           <Route exact path="/applicantPortal" component={ApplicantPortal} />
--- Common2/client/components/LandingPage/Homepage.jsx
+++ Common2/client/components/LandingPage/Homepage.jsx
@@ -1,24 +1,58 @@
 import React from 'react';
 import { useState } from 'react';
-import { Form, Button, Card, Container } from 'react-bootstrap'
-import { Switch, Route, Link, Redirect } from 'react-router-dom'
+import { Form, Button, Card, Container, Row, Col } from 'react-bootstrap'
+import { Switch, Route, Link, Redirect, useLocation } from 'react-router-dom'
 import './homepage.css';
 
 const Homepage = () => {
+  const location = useLocation();
+  console.log(location.pathname);
   return (
-      <div className='home'>
+      // <div className='home'>
 
-        <h1 className='title'>Apply.io</h1>
+      //   <h1 className='title'>Apply.io</h1>
 
-        <Link to='/signUp'>
+        // <Link to='/signUp'>
+        //   <Button>Create an Account</Button>
+        // </Link>
+
+        // <Link to="/login">
+        //   <Button>Log into Existing Account</Button>
+        // </Link>
+
+      // </div>
+
+
+<Container>
+  <Row style={{height: '400px', display: 'flex'
+ }}>
+   {/* backgroundColor: 'gray' */}
+    <Col style={{display:'flex', justifyContent: 'center', alignItems:'center', flexDirection: 'column', fontSize: '2.5em'}} xs={5}>
+      <div>Common App: A better way to start your career</div>
+      <div style={{width:'100%'}}>
+
+      <span>
+      <Link style={{display:'inline'}} to='/signUp'>
           <Button>Create an Account</Button>
         </Link>
+        </span>
 
-        <Link to="/login">
-          <Button>Log into Existing Account</Button>
+        <span>
+        <Link  style={{display:'inline'}} to="/login">
+          <Button>Log in</Button>
         </Link>
+        </span>
 
       </div>
+    </Col>
+    {/* <Col>2 of 2</Col> */}<Col><img style={{width:'100%'}} src='hero.jpg'/></Col>
+  </Row>
+  {/* <Row>
+    <Col>1 of 3</Col>
+    <Col>2 of 3</Col>
+    <Col>3 of 3</Col>
+  </Row> */}
+</Container>
   )
 }
 
