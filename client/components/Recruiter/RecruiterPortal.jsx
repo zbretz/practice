614d782babd62efd4ca35daa09500163d9da2e6c
Zach <zachary.bretz@gmail.com>
2022-01-31 05:42:35 -0700

    Integrated recruiter landing page

--- client/components/Recruiter/RecruiterPortal.jsx
+++ client/components/Recruiter/RecruiterPortal.jsx
@@ -1,23 +1,14 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { Switch, Route, Link, NavLink, useHistory } from 'react-router-dom'
+import React, { useRef } from 'react';
+import { Switch, Route, Link, NavLink, useHistory, useRouteMatch } from 'react-router-dom'
 import { Form, Button, Card, Container } from 'react-bootstrap'
 import { useAuth } from '../../contexts/AuthContext.js'
+import Recruiter_Landing from './Recruiter_Landing.jsx'
+import styles from '../styles/recruiterLanding.module.css'
 
 const RecruiterPortal = () => {
   const history = useHistory()
-  const { userSignOut, currentUser, userInfo, setUserInfo} = useAuth()
-  // const [currentUserInfo, setCurrentUserInfo] = useState()
-
-  useEffect(()=>{
-    //if userInfo is lost (for instance on refresh), retrieve user data from local storage
-    if (!userInfo){
-      let user_data = JSON.parse(localStorage.getItem('user_data'))
-      setUserInfo({
-        name: user_data.name,
-        email: user_data.email
-      })
-    }
-  }, [])
+  let match = useRouteMatch();
+  const { userSignOut} = useAuth()
 
   const handleSignOut = (e) => {
     e.preventDefault();
@@ -26,19 +17,20 @@ const RecruiterPortal = () => {
   }
 
   return (
-    // <Container>
-    //   <Button type="submit" onClick={handleSignOut}>Sign Out</Button>
-    //   <h1> Recruiter Portal </h1>
-    // </Container>
+    <>
     <Container>
-    <Button type="submit" onClick={handleSignOut}>Sign Out</Button>
-       <h1> Recruiter Portal </h1>
-      <div>
-      Hello {userInfo && userInfo.name} !
-      You are signed in as {userInfo && userInfo.email}.
-      You are a {userInfo.role}
-      </div>
-  </Container>
+      <Button type="submit" onClick={handleSignOut}>Sign Out</Button>
+      <h1> Recruiter Portal </h1>
+    </Container>
+
+    <Switch>
+
+      <Route path={`${match.path}/`}>
+        <Recruiter_Landing/>
+      </Route>
+
+    </Switch>
+    </>
   )
 }
 
---
+++ client/components/Recruiter/Recruiter_Candidate_View.jsx
@@ -0,0 +1,147 @@
+import React, { useRef, useState, useEffect } from 'react';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs'
+import Table from 'react-bootstrap/Table'
+import Container from 'react-bootstrap/Container'
+import styles from '../styles/recruiterCandidateView.module.css'
+import { BrowserRouter, Switch, Route, Link, Redirect, NavLink, useLocation, useParams } from 'react-router-dom'
+import queryString from 'query-string'
+import axios from 'axios';
+
+function ControlledTabs({data}) {
+  const [key, setKey] = useState('Profile');
+
+  console.log('zako!: ', data)
+
+  return (
+
+    <>
+    {/* test tabs */}
+    <Tabs
+      id="controlled-tab-example"
+      activeKey={key}
+      onSelect={(k) => setKey(k)}
+      className="mb-3"
+    >
+
+      {Object.entries(data).map(phase=>{
+console.log(phase[1])
+return(
+
+      <Tab eventKey={phase[1].tab} title={phase[1].tab}>
+      <h2>Frontend Interview (React)</h2>
+
+      {/* <Container> */}
+        <h5 style={{marginTop: "30px"}}  >Interviewer: {phase[1].interviewer_name}</h5>
+      {/* </Container> */}
+
+      <div>
+        <h5  style={{marginTop: "30px"}}>Performance Summary</h5>
+        <div>{phase[1].performance_summary}</div>
+      </div>
+
+      {/* <img src="./screenshot.png"/>
+      <img src={require('./screenshot.png')}/> */}
+
+      <div style={{width: "100%", height: "300px", backgroundColor:"black", marginTop: "30px"}}></div>
+
+      <h5 style={{marginTop: "30px"}} >Evaluation: </h5>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Excellent</th>
+            <th>Very Good</th>
+            <th>Good</th>
+            <th>Fair</th>
+          </tr>
+        </thead>
+        <tbody>
+            {/* <td>{Asked Qs when needed}</td> */}
+            {phase[1].evaluation.map(thing => {
+              return(
+                <tr>
+
+                <td>{thing.criterion}</td>
+                {
+                [4,3,2,1].map(num=> {
+                  if(num === thing.rating){
+                    return <td><span style={{backgroundColor:"black", borderRadius:"50%", height:"30px", width:"30px", display:"block", margin:'auto'}}></span></td>
+                  } else {
+                    return <td></td>
+                  }
+                })
+              }
+                </tr>
+
+              )
+            })}
+
+        </tbody>
+      </Table>
+
+      </Tab>
+
+)
+      })}
+
+
+    </Tabs>
+    {/* end test tabs */}
+
+
+    </>
+  );
+}
+
+const Recruiter_Candidate_View = () => {
+
+  // const { search } = useLocation()
+  // console.log(search)
+  // const values = queryString.parse(search)
+  // let user_id = values.id
+  let { id } = useParams();
+  let user_id = id
+  console.log(user_id)
+  const [data, setData] = useState()
+
+  useEffect(() => {
+
+  function getUserData() {
+      axios.get(`http://localhost:3000/applicants/${user_id}`)
+      // axios.get(`http://localhost:3000/recruiters/byId?id=${user_id}`)
+      .then(res => {
+        setData(res.data)
+        console.log(res.data)
+      })
+    }
+
+    if (user_id) {
+      getUserData();
+    }
+  }, [])
+
+  return (
+
+
+
+    <Container style={{width:"80%"}}
+>
+  {data &&
+    <>
+      <Container className={styles.candidateHeader}>
+        <h1>{data.user_name} <small className={styles.jobTitle}> Full Stack Engineer</small></h1>
+      </Container>
+      <Container>
+        <ControlledTabs data = {data.interview_phases} />
+      </Container>
+
+</>}
+    </Container>
+
+
+  )
+}
+
+export default Recruiter_Candidate_View;
