911857e78deb3815261fd138d794ce6f4368730c
Zach <zachary.bretz@gmail.com>
2022-01-30 17:37:55 -0700

    Created tabs for applicant vs recruiter login

--- Common2/client/components/LandingPage/Login.jsx
+++ Common2/client/components/LandingPage/Login.jsx
@@ -2,12 +2,17 @@ import React, { useRef, useState } from 'react';
 import { Form, Button, Card, Container } from 'react-bootstrap'
 import { useAuth } from '../../contexts/AuthContext.js'
 import { Switch, Route, Link, Redirect, NavLink, useHistory } from 'react-router-dom'
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs'
 
 const Login = () => {
   const history = useHistory()
   const emailRef = useRef()
   const passwordRef = useRef()
   const { signUp, role, isSignedIn, currentUser, userSignOut, signIn } = useAuth()
+
+  const [key, setKey] = useState('applicant');
+
 
   const handleSignIn = (e) => {
     e.preventDefault();
@@ -28,9 +33,20 @@ const Login = () => {
       <Button type="submit" onClick={handleSignOut}>Sign Out</Button>
 
       <>
+
+      <Tabs
+      id="controlled-tab-example"
+      activeKey={key}
+      onSelect={(k) => setKey(k)}
+      className="mb-3"
+    >
+
+      <Tab eventKey={'applicant'} title="Applicant Login">
+
+
         <Card>
           <Card.Body>
-            <h1>Log In</h1>
+            <h1>Applicant Log In</h1>
             {/* {JSON.stringify(currentUser)} */}
             <Form>
               <Form.Group id="email">
@@ -47,6 +63,30 @@ const Login = () => {
 
           </Card.Body>
         </Card>
+
+      </Tab>
+      <Tab eventKey={"recruiter"} title="Recruiter Login">
+          <Card>
+            <Card.Body>
+              <h1>Recruiter Log In</h1>
+              {/* {JSON.stringify(currentUser)} */}
+              <Form>
+                <Form.Group id="email">
+                  <Form.Label>Email</Form.Label>
+                  <Form.Control type="email" ref={emailRef} required />
+                </Form.Group>
+                <Form.Group id="password">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" ref={passwordRef} required />
+                </Form.Group>
+              </Form>
+
+              <Button type="button" onClick={handleSignIn}>Log In</Button>
+
+            </Card.Body>
+          </Card>
+        </Tab>
+      </Tabs>
       </>
       <div>
         Need to make an Account?
