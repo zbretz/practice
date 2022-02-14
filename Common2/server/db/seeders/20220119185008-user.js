118f9cee53b511c564eeace1c1bd8b4539408930
Zach <zachary.bretz@gmail.com>
2022-02-14 06:58:09 -0700

    Added second applicant

--- Common2/server/db/seeders/20220119185008-user.js
+++ Common2/server/db/seeders/20220119185008-user.js
@@ -20,6 +20,8 @@ module.exports = {
 		// await queryInterface.upsert('recruiters', {firebase_id: "GgaRWJL6UAQ6I75YHxltByzmshh2"}, {where: {
 		// {email:"winibhan@vehof.ne"}}
 		// )
+
+		//applicant1
 		await queryInterface.bulkInsert('users',
 			[{
 				id: '0246c9b0-07c1-46b1-98fd-2fba1b164d71',
@@ -36,6 +38,24 @@ module.exports = {
 			linkedIn: `linkedin.com/in/poophead`,
 			school: `$Clown University`
 		}],  {});
+
+		//applicant2
+		await queryInterface.bulkInsert('users',
+		[{
+			id: '19f20fd8-8dcd-45ed-abaa-1ef36d301c98',
+			email: 'poophead@test.com',
+			password: 'easypass',
+			role_id: 'applicant',
+			name: 'farty brain'
+		}],  {});
+
+		await queryInterface.bulkInsert('applicants',
+			[{
+				user_id: '19f20fd8-8dcd-45ed-abaa-1ef36d301c98',
+				experience: 3,
+				linkedIn: `linkedin.com/in/fartybrain`,
+				school: `$Clown University`
+			}],  {});
 
 
 		await queryInterface.bulkInsert('users',
