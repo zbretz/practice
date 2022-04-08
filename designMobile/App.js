543e847125c76bcabacf56e21aa6bfbace364e5a
zbretz <zachary.bretz@gmail.com>
2022-04-08 12:53:07 -0600

    Rendering video on both web and iOS

--- designMobile/App.js
+++ designMobile/App.js
@@ -1,30 +1,70 @@
 import React from 'react';
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider, Card, Text } from '@ui-kitten/components';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import Constants from 'expo-constants';
 import { Video, AVPlaybackStatus } from 'expo-av';
+import { Dimensions } from "react-native";
+
+var width = Dimensions.get('window').width; //full width
 
 
-export default function App () {
+export default function App() {
 
   const video = React.useRef(null);
   const [status, setStatus] = React.useState({});
 
+
   return (
-  <ApplicationProvider {...eva} theme={eva.light}>
-  <Card style={styles.card}>
-    <Text>
-    UI Kitten
-    </Text>
-  </Card>
-  
-  </ApplicationProvider>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <ScrollView >
+        <Card style={styles.card}>
+          <Video
+            ref={video}
+            style={styles.video}
+            source={{
+              uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+            }}
+            // source={require('./assets/SED.mp4')}
+            useNativeControls
+            resizeMode="contain"
+            isLooping
+            shouldPlay
+            isMuted
+            onPlaybackStatusUpdate={status => setStatus(() => status)}
+          />
+          <Text>
+            My Name
+          </Text>
+        </Card>
+        <Card style={styles.card}>
+          <Video
+            ref={video}
+            style={styles.video}
+            source={{
+              uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+            }}
+            useNativeControls
+            resizeMode="contain"
+            isLooping
+            shouldPlay
+            isMuted
+            onPlaybackStatusUpdate={status => setStatus(() => status)}
+          />
+          <Text>
+            My Name
+          </Text>
+        </Card>
+      </ScrollView>
+    </ApplicationProvider>
   )
 }
+
 const styles = StyleSheet.create({
   card: {
-    margin: 10
+    margin: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
   container: {
     flex: 1,
@@ -39,4 +79,8 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
   },
+  video: {
+    width: width - 40,
+    height: (width - 40) * .5635,
+  }
 });
