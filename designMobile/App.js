703b615fc6414491ae9150788347c273d92bd0b0
zbretz <zachary.bretz@gmail.com>
2022-04-08 13:34:41 -0600

    Looping over cards for rendering

--- designMobile/App.js
+++ designMobile/App.js
@@ -8,53 +8,39 @@ import { Dimensions } from "react-native";
 
 var width = Dimensions.get('window').width; //full width
 
+var card_count = 5
+
 
 export default function App() {
 
   const video = React.useRef(null);
   const [status, setStatus] = React.useState({});
 
-
   return (
     <ApplicationProvider {...eva} theme={eva.light}>
       <ScrollView >
-        <Card style={styles.card}>
-          <Video
-            ref={video}
-            style={styles.video}
-            source={{
-              uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-            }}
-            // source={require('./assets/SED.mp4')}
-            useNativeControls
-            resizeMode="contain"
-            isLooping
-            shouldPlay
-            isMuted
-            onPlaybackStatusUpdate={status => setStatus(() => status)}
-          />
-          <Text>
-            My Name
-          </Text>
-        </Card>
-        <Card style={styles.card}>
-          <Video
-            ref={video}
-            style={styles.video}
-            source={{
-              uri: 'https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
-            }}
-            useNativeControls
-            resizeMode="contain"
-            isLooping
-            shouldPlay
-            isMuted
-            onPlaybackStatusUpdate={status => setStatus(() => status)}
-          />
-          <Text>
-            My Name
-          </Text>
-        </Card>
+
+        {[...Array(2)].map((e, i) => {
+          return (
+            <Card key={i} style={styles.card}>
+              <Video
+                ref={video}
+                style={styles.video}
+                source={{
+                  uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+                }}
+                // source={require('./assets/SED.mp4')}
+                useNativeControls
+                resizeMode="contain"
+                isLooping
+                shouldPlay
+                isMuted
+                onPlaybackStatusUpdate={status => setStatus(() => status)}
+              />
+            </Card>
+          )
+        })}
+
       </ScrollView>
     </ApplicationProvider>
   )
@@ -80,7 +66,11 @@ const styles = StyleSheet.create({
     textAlign: 'center',
   },
   video: {
-    width: width - 40,
-    height: (width - 40) * .5635,
+    // width: width - 40,
+    // height: (width - 40) * .5635,
+    // borderColor: 'blue',
+    // borderWidth: 4
+    width: width * .8,
+    height: width * .45
   }
 });
