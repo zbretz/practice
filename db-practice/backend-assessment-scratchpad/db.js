88d9798e0b32cf5fa109dbc05ed374814f4fd131
zbretz <zachary.bretz@gmail.com>
2022-05-10 11:08:13 -0600

    'Create table' query is functional

--- db-practice/backend-assessment-scratchpad/db.js
+++ db-practice/backend-assessment-scratchpad/db.js
@@ -1,13 +1,30 @@
 const { Pool, Client } = require('pg')
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: null,
-  port: 5432,
+    user: 'postgres',
+    host: 'localhost',
+    database: 'scratchpad',
+    password: null,
+    port: 5432,
 })
 
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
+// pool.query('SELECT NOW()', (err, res) => {
+//   console.log(err, res)
+//   pool.end()
+// })
+
+const create_table = () => {
+    pool.query(
+        `CREATE TABLE patient(
+            ID int NOT NULL,
+            name varchar(64) NOT NULL,
+            PRIMARY KEY (ID)
+        )`)
+        .then(res => {
+            console.log(res);
+        })
+        .catch(e => {
+            console.error(e)
+        })
+}
+
+create_table()
